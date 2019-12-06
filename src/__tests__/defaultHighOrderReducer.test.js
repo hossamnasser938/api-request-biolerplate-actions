eabe7d6a1f8f7205b861fb85a8fabf4375ae26c1
Hossam <hossamnasser938@gmail.com>
2019-12-06 20:08:34 +0200

    test defaultHighOrderReducer

---
+++ src/__tests__/defaultHighOrderReducer.test.js
@@ -0,0 +1,107 @@
+import { defaultHighOrderReducer } from "../defaultHighOrderReducer";
+
+test("test defaultHighOrderReducer", () => {
+  // ___ test ordinary reducer ___ //
+  const reducer = (state = {}, { type, payload }) => {
+    switch (type) {
+      case "SUCCESS_BASE_ACTION":
+        return {
+          success: payload
+        };
+
+      case "SUCCESS_BASE_ACTION_1":
+        return {
+          success: payload
+        };
+
+      default:
+        return state;
+    }
+  };
+
+  expect(
+    reducer({}, { type: "SUCCESS_BASE_ACTION", payload: { message: "done" } })
+  ).toEqual({
+    success: {
+      message: "done"
+    }
+  });
+
+  expect(reducer({}, { type: "START_BASE_ACTION_1" })).toEqual({});
+
+  expect(reducer({}, { type: "STOP_BASE_ACTION_1" })).toEqual({});
+
+  expect(
+    reducer({}, { type: "ERROR_BASE_ACTION_1", payload: { message: "error" } })
+  ).toEqual({});
+
+  expect(
+    reducer({}, { type: "SUCCESS_BASE_ACTION_1", payload: { message: "done" } })
+  ).toEqual({
+    success: {
+      message: "done"
+    }
+  });
+
+  expect(reducer({}, { type: "START_BASE_ACTION" })).toEqual({});
+
+  expect(reducer({}, { type: "STOP_BASE_ACTION" })).toEqual({});
+
+  expect(
+    reducer({}, { type: "ERROR_BASE_ACTION", payload: { message: "error" } })
+  ).toEqual({});
+
+  // ___ test reducer after being passed to defaultHighOrderReducer ___ //
+
+  const defaultReducer = defaultHighOrderReducer(
+    {},
+    ["BASE_ACTION", "BASE_ACTION_1"],
+    reducer
+  );
+
+  expect(
+    reducer({}, { type: "SUCCESS_BASE_ACTION", payload: { message: "done" } })
+  ).toEqual({
+    success: {
+      message: "done"
+    }
+  });
+
+  expect(defaultReducer({}, { type: "START_BASE_ACTION" })).toEqual({
+    BASE_ACTIONLoading: true
+  });
+
+  expect(defaultReducer({}, { type: "STOP_BASE_ACTION" })).toEqual({
+    BASE_ACTIONLoading: false
+  });
+
+  expect(
+    defaultReducer(
+      {},
+      { type: "ERROR_BASE_ACTION", payload: { message: "error" } }
+    )
+  ).toEqual({ BASE_ACTIONError: { message: "error" } });
+
+  expect(
+    reducer({}, { type: "SUCCESS_BASE_ACTION_1", payload: { message: "done" } })
+  ).toEqual({
+    success: {
+      message: "done"
+    }
+  });
+
+  expect(defaultReducer({}, { type: "START_BASE_ACTION_1" })).toEqual({
+    BASE_ACTION_1Loading: true
+  });
+
+  expect(defaultReducer({}, { type: "STOP_BASE_ACTION_1" })).toEqual({
+    BASE_ACTION_1Loading: false
+  });
+
+  expect(
+    defaultReducer(
+      {},
+      { type: "ERROR_BASE_ACTION_1", payload: { message: "error" } }
+    )
+  ).toEqual({ BASE_ACTION_1Error: { message: "error" } });
+});
