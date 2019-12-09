4517ced73de9242a35b38c3822fd7513ce8519a5
Hossam <hossamnasser938@gmail.com>
2019-12-09 18:09:51 +0200

    test configAxiosInterceptors

---
+++ src/__tests__/configAxiosInterceptors.test.js
@@ -0,0 +1,16 @@
+import { configAxiosInterceptors } from "../configAxiosInterceptors";
+import axios from "axios";
+
+test("test configAxiosInterceptors", () => {
+  expect(axios.interceptors.request.handlers[0]).toBeUndefined();
+  expect(axios.interceptors.response.handlers[0]).toBeUndefined();
+
+  configAxiosInterceptors(() => {}, "", []);
+
+  expect(axios.interceptors.request.handlers[0]).toBeDefined();
+  expect(axios.interceptors.request.handlers[0].fulfilled).toBeDefined();
+  expect(axios.interceptors.request.handlers[0].rejected).toBeDefined();
+  expect(axios.interceptors.response.handlers[0]).toBeDefined();
+  expect(axios.interceptors.response.handlers[0].fulfilled).toBeDefined();
+  expect(axios.interceptors.response.handlers[0].rejected).toBeDefined();
+});
